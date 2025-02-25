3f3b99e5a2afaec4c1c5d7ca5d2682f559e1fb7d
semma7 <emmaj.solyom@gmail.com>
2025-02-25 21:11:26 +0100

    correction

--- ImageEditor/Views/MainWindow.axaml.cs
+++ ImageEditor/Views/MainWindow.axaml.cs
@@ -6,23 +6,7 @@ namespace ImageEditor.Views;
 public partial class MainWindow : Window
 {
     public MainWindow()
-    {
-
-        try
-        {
-            // Read a text file line by line.
-            string[] lines = File.ReadAllLines("../image.txt");
-
-            image = new Image(lines[0], lines[1]);
-            Console.WriteLine(image.FileName);
-            var filename = image.FileName;
-
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error reading file: {ex.Message}");
-        }
-     
-        //InitializeComponent();
+    {     
+        InitializeComponent();
     }
 }
