c25d41347db3d9bbb10320b5cb2739f461585480
gabija <gasta24@student.sdu.dk>
2025-02-26 21:40:29 +0100

    Managed to save the file name after closing the application, though we should look into how to make it more efficient, cause it will make a new file every time we change the name

--- ImageEditor/ViewModels/MainWindowViewModel.cs
+++ ImageEditor/ViewModels/MainWindowViewModel.cs
@@ -10,35 +10,53 @@ namespace ImageEditor.ViewModels;
 public partial class MainWindowViewModel : ViewModelBase
 {
     public ImageViewModel Image { get; set; }
-    
+
     private string _title = "ImageEditor";
-        public string Title
-        {
-            get => _title;
-            set => SetProperty(ref _title, value);
-        }
+    public string Title
+    {
+        get => _title;
+        set => SetProperty(ref _title, value);
+    }
+    private string _fileName = "default_name";
+
+    public string FileName
+    {
+        get => _fileName;
+        set => SetProperty(ref _fileName, value);
+    }
 
     private bool _isEdited = false;
 
-        public ICommand SaveCommand { get; }
+    public ICommand SaveCommand { get; }
 
-        public MainWindowViewModel()
+    public MainWindowViewModel()
+{
+    try
     {
-        try
+        // Load the last saved file name
+        if (File.Exists("../last_filename.txt"))
         {
-            string[] lines = File.ReadAllLines("../image.txt");
-            Image = new ImageViewModel(lines[0], lines[1], this); // Pass `this`
+            FileName = File.ReadAllText("../last_filename.txt");
         }
-        catch (Exception ex)
+        else
         {
-            Console.WriteLine($"Error reading file: {ex.Message}");
+            FileName = "default_name";
         }
 
-        SaveCommand = new RelayCommand(SaveFile);
+        string[] lines = File.ReadAllLines("../image.txt");
+        Image = new ImageViewModel(lines[0], lines[1], this);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error reading file: {ex.Message}");
     }
 
-        public void MarkAsEdited()
-        {
+    SaveCommand = new RelayCommand(SaveFile);
+}
+
+
+    public void MarkAsEdited()
+    {
         if (!_isEdited)
         {
             Title += "*"; // Append * to indicate unsaved changes
@@ -46,24 +64,37 @@ public partial class MainWindowViewModel : ViewModelBase
         }
     }
 
-    private void SaveFile()
+private void SaveFile()
+{
+    if (Image == null) return;
+
+    try
     {
-        if (Image == null) return;
-
-        try
+        if (string.IsNullOrWhiteSpace(FileName))
         {
-            string filePath = "../image.txt";
-            string size = $"{Image.Width} {Image.Height}";
-            string pixelData = string.Join("", Image.Pixels.Select(p => p.Value.ToString()));
-
-            File.WriteAllLines(filePath, new[] { size, pixelData });
-
-            Title = "ImageEditor"; // Reset title after saving
-            _isEdited = false;
+            FileName = "default_name"; 
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error saving file: {ex.Message}");
-        }
+
+        string filePath = $"../{FileName}.txt";
+        string size = $"{Image.Width} {Image.Height}";
+        string pixelData = string.Join("", Image.Pixels.Select(p => p.Value.ToString()));
+
+        // Save image data
+        File.WriteAllLines(filePath, new[] { size, pixelData });
+
+        // Save the file name to a settings file
+        File.WriteAllText("../last_filename.txt", FileName);
+
+        Title = "ImageEditor"; 
+        _isEdited = false;
     }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error saving file: {ex.Message}");
+    }
+}
+
+
+
+
 }
