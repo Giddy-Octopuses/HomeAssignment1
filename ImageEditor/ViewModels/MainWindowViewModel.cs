a6de21fc38a1df1caa8588810790d1515f3e1b17
gabija <gasta24@student.sdu.dk>
2025-02-26 21:06:41 +0100

    added few libraries - AI suggested, added property, on line 30 added this, cause it keeped complaining, corrected/added methods like: MarkAsEdited, SaveFile

--- ImageEditor/ViewModels/MainWindowViewModel.cs
+++ ImageEditor/ViewModels/MainWindowViewModel.cs
@@ -1,24 +1,69 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.IO;
+using System.Windows.Input;
+using System.Linq;
+
 
 namespace ImageEditor.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public ImageViewModel Image { get; set; }
+    
+    private string _title = "ImageEditor";
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
 
-    public MainWindowViewModel()
+    private bool _isEdited = false;
+
+        public ICommand SaveCommand { get; }
+
+        public MainWindowViewModel()
     {
         try
         {
             string[] lines = File.ReadAllLines("../image.txt");
-
-            Image = new ImageViewModel(lines[0], lines[1]);
+            Image = new ImageViewModel(lines[0], lines[1], this); // Pass `this`
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error reading file: {ex.Message}");
         }
+
+        SaveCommand = new RelayCommand(SaveFile);
     }
-    
+
+        public void MarkAsEdited()
+        {
+        if (!_isEdited)
+        {
+            Title += "*"; // Append * to indicate unsaved changes
+            _isEdited = true;
+        }
+    }
+
+    private void SaveFile()
+    {
+        if (Image == null) return;
+
+        try
+        {
+            string filePath = "../image.txt";
+            string size = $"{Image.Width} {Image.Height}";
+            string pixelData = string.Join("", Image.Pixels.Select(p => p.Value.ToString()));
+
+            File.WriteAllLines(filePath, new[] { size, pixelData });
+
+            Title = "ImageEditor"; // Reset title after saving
+            _isEdited = false;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error saving file: {ex.Message}");
+        }
+    }
 }
