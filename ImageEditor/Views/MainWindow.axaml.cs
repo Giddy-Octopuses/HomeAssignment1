e26a2885a41117db1dd93b2060fae8f3e300dc83
AnnalenaLorenzen <lorenzenannalena@web.de>
2025-03-01 11:38:53 +0100

    saving

--- ImageEditor/Views/MainWindow.axaml.cs
+++ ImageEditor/Views/MainWindow.axaml.cs
@@ -3,6 +3,7 @@ using ImageEditor.ViewModels;
 using Avalonia.Interactivity;
 using System.IO;
 using System;
+using System.Linq;
 
 
 namespace ImageEditor.Views;
@@ -16,7 +17,7 @@ public partial class MainWindow : Window
     }
 
     // This happens when you click 'load'
-    public void ClickHandler(object sender, RoutedEventArgs args)
+    public void LoadHandler(object sender, RoutedEventArgs args)
     {
         try
         {
@@ -26,9 +27,52 @@ public partial class MainWindow : Window
             {
                 viewModel.Image = new ImageViewModel(lines[0], lines[1]) // Update the existing ImageViewModel
                 {
-                    FileNameText = "image.txt"
+                    FileNameText = "image"
                 };
                 message.Text = "The .txt file is now loaded!";
+            }
+            else
+            {
+                Console.WriteLine("Error: DataContext is not set or is of the wrong type.");
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading file: {ex.Message}");
+        }
+    }
+
+    public void SaveHandler(object sender, RoutedEventArgs args)
+    {
+        try
+        {
+            // Get the ViewModel from DataContext
+            if (DataContext is MainWindowViewModel viewModel)
+            {
+
+                try
+                {
+                    if (string.IsNullOrWhiteSpace(viewModel.Image.FileNameText))
+                    {
+                        viewModel.Image.FileNameText = "image";
+                    }
+
+                    string filePath = $"../Images/{viewModel.Image.FileNameText}.txt";
+                    string size = $"{viewModel.Image.Height} {viewModel.Image.Width}";
+                    string pixelData = string.Join("", viewModel.Image.Pixels.Select(Value => Value.Value.ToString()));
+
+                    // Save image data
+                    File.WriteAllLines(filePath, new[] { size, pixelData });
+
+                    //Title = "ImageEditor";
+                    //_isEdited = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error saving file: {ex.Message}");
+                }
+                message.Text = "The .txt file is now saved!";
             }
             else
             {
